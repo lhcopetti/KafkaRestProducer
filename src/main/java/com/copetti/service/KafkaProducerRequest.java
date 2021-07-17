bb49cc33bcf88b5b6ab5748071d0667c02a70ab4
Luís Copetti <luis.copetti@picpay.com>
2021-07-17 12:04:41 -0300

    Add support for supplying headers as key value string pairs

--- src/main/java/com/copetti/service/KafkaProducerRequest.java
+++ src/main/java/com/copetti/service/KafkaProducerRequest.java
@@ -3,6 +3,8 @@ package com.copetti.service;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Getter
 public class KafkaProducerRequest {
@@ -10,4 +12,5 @@ public class KafkaProducerRequest {
     private final String topicName;
     private final String brokerList;
     private final Object value;
+    private final Map<String, String> headers;
 }
